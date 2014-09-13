4114d94e321b7579106a4f81d4f3c2ef690c1263
imann24 <imann24@gmail.com>
2014-09-13 11:05:25 -0400

    Character Timers Complete (for now)

--- Unity Project/Assets/Characters/PlayerScripts/CharacterTimers.cs
+++ Unity Project/Assets/Characters/PlayerScripts/CharacterTimers.cs
@@ -3,22 +3,27 @@ using System.Collections;
 
 public class CharacterTimers : MonoBehaviour {
 	//variables to control the character's current state
+	public bool something = true;
 	public int hungerLevel;
-	public int waitingTime;
 	public float waitTimer;
-	public int hungerTime;
-	public float hungerTimer;
-	public int eatingTime;
-	public float scoreMultiplier;
+	public float digestingTimer;
+	public float eatingTimer;
 	public string state;
 	public int stateNum = 0;
-	public int maxStates = 4;
+	public int maxStates = 5;
 	public string [] states;
 	public Material inactive;
+	//variables to control character's appearance 
+	public Vector3 full; 
+
+	//variables to track the character's eating 
+	public int wordsEaten;
+	public float scoreMultiplier = 1;
 
 	//script reference to the main controller script
 	public GameObject gameController;
 	VariableControl variables;
+
 	// Use this for initialization
 	void Start () {
 		//establishes script reference 
@@ -29,35 +34,61 @@ public class CharacterTimers : MonoBehaviour {
 		states[0] = "NONE";
 		states[1] = "WAITING";
 		states[2] = "EATING";
-		states[3] = "IMPATIENT";
+		states[3] = "FULL";
+		states[4] = "IMPATIENT";
 
 		//defines the time the character remains in the states based on the controller script
-		waitingTime = variables.maxWaitingTime;
-		waitTimer = (float) variables.maxWaitingTime;
-		hungerTime = variables.maxHungerTime;
+		fillTimers();
+		//defines variables that relate to character appearance 
+		full = new Vector3 (2, 2, 2);
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//updates the character's state
-		updateState();
-
-		//counts down the characters waiting timer while it is not being fed
-		if (state == states[1]) {
-			waitTimer -= Time.deltaTime;
-			if ((int)waitTimer < waitingTime && waitingTime > 0) {
-				waitingTime--; 
+		if (Application.loadedLevelName == "Phase2") {
+			//updates the character's state
+			updateState();
+			scoreMultiplier = variables.setScoreMultiplier (wordsEaten, scoreMultiplier);
+			//counts down the characters waiting timer while it is not being fed
+			if (state == states[1]) {
+				waitTimer -= Time.deltaTime;
+			} else if (state == states[2]) { //counts down while the character is eating
+				eatingTimer -= Time.deltaTime;
+				eating ();
+			} else if (state == states[3]) { //counts down after the character has eaten
+				digestWord();
 			}
-		}
 
-		//sets the character to "IMPATIENT" if its waiting timer has ellapsed 
-		if (waitingTime == 0) {
-			gameObject.transform.renderer.material = inactive;
-			stateNum = 3;
+			//sets the character to "IMPATIENT" if its waiting timer has ellapsed 
+			if ((int) waitTimer <= 0) { //renders the character unable to recieve more food if it has waited too long
+				gameObject.transform.renderer.material = inactive;
+				stateNum = 4;
+				fillTimers();
+			} else if ((int) eatingTimer <= 0) { //sets the character to digest when it is done eating
+				stateNum = 3;
+				fillTimers();
+			} else if ((int) digestingTimer <= 0) { //sets the character back to waiting once it is done digesting
+				stateNum = 1;
+				gameObject.transform.rotation = Quaternion.identity;
+				fillTimers();
+			}
 		}
 	}
 
+	void OnMouseDown () {
+		if (Application.loadedLevelName == "Phase2" && variables.wordSelected) { //feeds the character a word, if valid
+			if (state == states[1]) {
+				eatWord();
+				variables.wordSelected = false;
+				if (wordsEaten < 10) { //tracks the amount of words eaten
+					wordsEaten++;
+				}
+				//increases score
+				variables.score += scoreMultiplier;
+			}
+		}
+	}
 	void updateState() {
 		//changes the character's state if the game has just loaded "Phase2"
 		if (Application.loadedLevelName == "Phase2" && stateNum == 0) {
@@ -68,4 +99,32 @@ public class CharacterTimers : MonoBehaviour {
 		gameObject.transform.tag = state;
 
 	}
+
+	//sets the state and restores character to normal size
+	void eatWord() {
+		fillTimers();
+		stateNum = 2;
+		gameObject.transform.localScale = full;
+	}
+
+	//causes character to swell and slowly shrink as it eats
+	void eating () {
+		float shrinkRate = 2.5F/eatingTimer; 
+		if (shrinkRate < 1) {
+			gameObject.transform.localScale = full - new Vector3 (shrinkRate, shrinkRate, shrinkRate);
+		}
+	}
+
+	//causes character to rotate while digesting
+	void digestWord() {
+		digestingTimer -= Time.deltaTime;
+		gameObject.transform.Rotate(0, -Time.deltaTime*300, 0, Space.World);	
+	}
+
+	//resets all timers
+	void fillTimers () {
+		waitTimer = (float) (variables.maxWaitingTime - 10 + wordsEaten);
+		eatingTimer = (float) (variables.maxEatingTime - wordsEaten);
+		digestingTimer = (float) (variables.maxHungerTime - wordsEaten);
+	}	
 }
--- Unity Project/Assets/GameController/GameController Scripts/VariableControl.cs
+++ Unity Project/Assets/GameController/GameController Scripts/VariableControl.cs
@@ -8,8 +8,11 @@ public class VariableControl : MonoBehaviour {
 	public int characterSelectNum = 3;
 	public int maxFed = 10;
 	public int maxWaitingTime = 20;
+	public int minWaitingTime = 10;
 	public int maxHungerTime = 15;
+	public int minHungerTime = 5;
 	public int maxEatingTime = 12;
+	public int minEatingTime = 2;
 	public int maxMultiplier = 5;
 
 	//character variables 
@@ -19,11 +22,13 @@ public class VariableControl : MonoBehaviour {
 	public int currentCharacterSelectNum = 0;
 
 	//gamesate variables
-	public int score;
+	public float score;
 	public float gameTimer;
 	public bool timeToChangeGameState;
 	public bool timeToEndGame;
 
+	//word variables 
+	public bool wordSelected = false;
 	// Use this for initialization
 	void Awake() {
 		//preserves object between loads 
@@ -62,6 +67,20 @@ public class VariableControl : MonoBehaviour {
 			currentCharacterSelectNum = 1;
 		} else if (characterSelected[2] == false) {
 			currentCharacterSelectNum = 2;
+		}
+	}
+
+	//function to set the score of a character
+	public float setScoreMultiplier (int wordsEaten, float multiplier) {
+		if (wordsEaten < 9) {
+			print ("inc 1");
+			return (float) (((float)wordsEaten)/4 + 1);
+		} else if (wordsEaten == 9) {
+			return 4.0F;
+		} else if (wordsEaten == maxFed) {
+			return (float) maxMultiplier;
+		} else {
+			return 1;
 		}
 	}
 }
