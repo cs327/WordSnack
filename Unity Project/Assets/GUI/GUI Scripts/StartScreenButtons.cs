972e8565a3653b799a30a9b76daedbb3be705a9f
Ning Xie <ningxie1991@gmail.com>
2014-09-22 19:03:58 -0400

    changed the start screen code to implement ngui

---
+++ Unity Project/Assets/GUI/GUI Scripts/StartScreenButtons.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScreenButtons : UIButton{
+
+	public string ButtonName;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	protected override void OnPress (bool isPressed) {
+		if(ButtonName == "play"){
+			Application.LoadLevel("CharacterSelect");
+		}else if(ButtonName == "instructions"){
+			Debug.Log ("clicked instructions");
+		}else if (ButtonName == "About"){
+			Debug.Log ("clicked about");
+		}
+	}
+}
