d461b8410f8eec870103c138e1e10d7265e7a5cb
Public User <publicuser@ashndrome-01.hampshire.edu>
2014-09-24 20:26:47 -0400

    Added the start of the character class and set up the taste system using Delegates -Josiah and David

---
+++ Unity Project/Assets/Characters/PlayerScripts/Character.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CharacterClass {
+	private List<TasteCollection.Taste> myTastes;
+	public CharacterTimers Timer;
+
+	public CharacterClass(List<TasteCollection.Taste> myTastes)
+	{
+		this.myTastes = myTastes;
+		//Timer = new CharacterTimers ();
+
+	}
+
+	public int Likes(string word) 
+	{
+		int satisfied = 0;
+		int bonus = 0;
+		foreach (TasteCollection.Taste t in myTastes) {
+			if (satisfied > 0 && t(word) > 0)			
+				bonus++;
+			
+			satisfied += t(word);
+		}
+		return satisfied + bonus;
+	}
+
+	public void AddTaste(TasteCollection.Taste taste)
+	{
+		myTastes.Add(taste);
+	}
+	public void AddTaste(List<TasteCollection.Taste> tastes)
+	{
+		foreach(TasteCollection.Taste t in tastes)
+			if(!myTastes.Contains(t))
+				myTastes.Add(t);
+	}
+
+	public void RemoveTaste(TasteCollection.Taste taste)
+	{
+		if (myTastes.Contains (taste))
+			myTastes.Remove (taste);
+	}
+	
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Unity Project/Assets/Characters/PlayerScripts/Tastes.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+
+public static class TasteCollection
+{
+	public delegate int Taste(string word);
+
+
+	public static int WordsLongerThan5(string word)
+	{
+		return (word.Length <= 5)? 0 : word.Length - 5;
+	}
+	public static int ThreeLetterWords(string word)
+	{
+				if (word.Length == 3)
+						return 1;
+				else
+						return 0;
+	}
+}
+
+/*
+ * Very vague idea of approximately how to use this - Josiah
+ * 
+ * 			List<TasteCollection.Taste> myTastes = new List<TasteCollection.Taste>(); * 
+			myTastes.Add(TasteCollection.WordsLongerThan5); //Any valid taste
+
+			int answer = myTastes[0]("Words");
+
+
+			or Do this:
+			
+			List<TasteCollection.Taste> myTastes = new List<TasteCollection.Taste>(); * 
+			myTastes.Add(TasteCollection.WordsLongerThan5); //Any valid taste
+			Character character = ....
+			Taste taste = TasteCollection.ThreeLetterWords;
+			character.AddTaste(taste);
+			These are just examples for clarity - the character class's "Likes" function does this
+			for you - just call it on a character
+			*/
