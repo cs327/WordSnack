eda00ab09b9f97e29f2b8a3dd273518c736b070a
OwenNeuburger <OwenNeuburger@hampguest79-102.hampshire.edu>
2014-10-04 16:55:11 -0400

    migrated tuning variables for letters to Variablecontrol

--- Unity Project/Assets/GameController/GameController Scripts/VariableControl.cs
+++ Unity Project/Assets/GameController/GameController Scripts/VariableControl.cs
@@ -4,6 +4,11 @@ using System.Collections;
 public class VariableControl : MonoBehaviour {
 	//main game variables: 
 
+	//letter tuning variables
+	public int numA,numB,numC,numD,numE,numF,numG,numH,numI,numJ,numK,numL,numM,numN,numO,numP,numQ,numR,numS,numT,numU,numV,numW,numX,numY,numZ;
+	public int totalLetters,totalVowels;
+
+
 	//tweakable variables: 
 	public int characterSelectNum = 2;
 	public int maxFed = 10;
@@ -39,6 +44,9 @@ public class VariableControl : MonoBehaviour {
 	}
 
 	void Start () {
+		//sets tuning variables for letters
+		TuningList();
+
 		//creates the arrays to handle character selection 
 		characterSelected = new bool[characterSelectNum];
 		selectedCharacters = new GameObject[characterSelectNum];
@@ -60,5 +68,36 @@ public class VariableControl : MonoBehaviour {
 		} else {
 			timeToChangeGameState = false;
 		}
+	}
+
+	void TuningList(){
+		numA = 9;
+		numB = 2;
+		numC = 2;
+		numD = 4;
+		numE = 12;
+		numF = 2;
+		numG = 3;
+		numH = 2;
+		numI = 9;
+		numJ = 1;
+		numK = 1;
+		numL = 4;
+		numM = 2;
+		numN = 6;
+		numO = 8;
+		numP = 2;
+		numQ = 1;
+		numR = 6;
+		numX = 1;
+		numT = 6;
+		numU = 4;
+		numV = 2;
+		numW = 2;
+		numX = 1;
+		numY = 2;
+		numZ = 1;
+		totalLetters =  numA + numB + numC + numD + numE + numF + numG + numH + numI + numJ + numK + numL + numM + numN + numO + numP + numQ + numR + numS + numT + numU + numV + numW + numX + numY + numZ;
+		totalVowels = numA + numE + numI + numO + numU;
 	}
 }
