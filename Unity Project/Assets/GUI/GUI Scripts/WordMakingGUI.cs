115557086087ab1a8e79b27d899df86c1cd663eb
Ning Xie <ningxie1991@gmail.com>
2014-10-05 18:08:26 -0400

    added tiles remaining indication

--- Unity Project/Assets/GUI/GUI Scripts/WordMakingGUI.cs
+++ Unity Project/Assets/GUI/GUI Scripts/WordMakingGUI.cs
@@ -24,6 +24,13 @@ public class WordMakingGUI : MonoBehaviour {
 		style.normal.background = null;
 		GUI.Label (new Rect (Screen.width * 0.008f, Screen.height*0.018f, Screen.width*0.18f, Screen.height*0.08f), "\tScore:  " + GameObject.Find("GameController").GetComponent<wordBuildingController>().variables.score, style);
 
+		// tiles remaining
+		style.normal.background = background;
+		GUI.Label (new Rect (Screen.width * 0.32f, Screen.height*0.01f, Screen.width*0.38f, Screen.height*0.08f), "", style);
+		style.normal.background = null;
+		GUI.Label (new Rect (Screen.width * 0.34f, Screen.height*0.018f, Screen.width*0.35f, Screen.height*0.08f), "Tiles Remaining:  " + GameObject.Find("GameController").GetComponent<VariableControl>().totalLetters, style);
+
+
 		// menu button
 		style.normal.background = background;
 		GUI.Label (new Rect (Screen.width * 0.79f, Screen.height*0.01f, Screen.width*0.2f, Screen.height*0.08f), "", style);
