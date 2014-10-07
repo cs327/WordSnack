985ba3d8e11edb53ed7de7907db3c8ca0a30fd77
imann24 <imann24@gmail.com>
2014-10-06 22:43:51 -0400

    Set Screen Rotation to Landscape Only
    - Also added characters to start screen, but are hidden behind GUI.

--- Unity Project/Assets/Characters/PlayerScripts/Character.cs
+++ Unity Project/Assets/Characters/PlayerScripts/Character.cs
@@ -77,70 +77,72 @@ public class Character : MonoBehaviour
     void Start()
     {
         //get the same variables everyone else is using
-        variables = GameObject.Find("GameController").GetComponent<VariableControl>();
+		if (Application.loadedLevelName != "StartScreenTest") {
+			variables = GameObject.Find("GameController").GetComponent<VariableControl>();
         
-        if (tasteDictionary == null) //We only need (or can have, since it's static) one copy of this game-wide, so if it's been done already, don't do it again
-        {
-            tasteDictionary = new Dictionary<int, TasteCollection.Taste>();
-            //Create the dictionary of taste ID's to functions
-            tasteDictionary.Add(0, TasteCollection.threeLetters);
-            tasteDictionary.Add(1, TasteCollection.fiveOrLonger);
-            tasteDictionary.Add(2, TasteCollection.unCommonLetters);
-            tasteDictionary.Add(3, TasteCollection.endsWithVowel);
-            tasteDictionary.Add(4, TasteCollection.twoOrMoreVowels);
-            tasteDictionary.Add(5, TasteCollection.twoOrMoreSame);
-            tasteDictionary.Add(6, TasteCollection.startsWithVowel);
-            tasteDictionary.Add(7, TasteCollection.startsAndEndsWithSame);
-            tasteDictionary.Add(8, TasteCollection.fourLetters);
-            tasteDictionary.Add(9, TasteCollection.noPreference);
-            tasteDictionary.Add(10, TasteCollection.trashCollection);
-        }
-		if (humanReadableTasteDictionary == null) { //We only need (or can have, since it's static) one copy of this game-wide, so if it's been done already, don't do it again
-			humanReadableTasteDictionary = new Dictionary<int, string> ();
-			//Create the dictionary of taste ID's to Human-readable text
-			humanReadableTasteDictionary.Add (0, "three letters");
-			humanReadableTasteDictionary.Add (1, "five letters or more");
-			humanReadableTasteDictionary.Add (2, "uncommon letters(F,H,V,W,Y,K,J,X,Q,Z)");
-			humanReadableTasteDictionary.Add (3, "ends in a vowel");
-			humanReadableTasteDictionary.Add (4, "more than one vowel");
-			humanReadableTasteDictionary.Add (5, "two or more of the same letter");
-			humanReadableTasteDictionary.Add (6, "starts with a vowel");
-			humanReadableTasteDictionary.Add (7, "starts and ends with same");
-			humanReadableTasteDictionary.Add (8, "four letters");
-			humanReadableTasteDictionary.Add (9, "anything");
-			humanReadableTasteDictionary.Add (10, "trash - things that aren't words");
-				}
-		myTastes = new List<TasteCollection.Taste>();
-		//We have to do this all the time now, in order to display the tastes correctly.
-		//{
-			//initialize the tastes for the characters
-            //First make a generic list of the character taste arrays so that I can
-            //easily access my tastes with my character number
-            List<int[]> characterTastes = new List<int[]>();
-            characterTastes.Add(new int[] { 10 }); //The trash character, ID 0, has one taste, taste 10
-            characterTastes.Add(variables.TastesForCharacter1);
-            characterTastes.Add(variables.TastesForCharacter2);
-            characterTastes.Add(variables.TastesForCharacter3);
-            characterTastes.Add(variables.TastesForCharacter4);
-            characterTastes.Add(variables.TastesForCharacter5);
-            //now we add an arbitrary number of tastes
-			//also we set up the text to be displayed for the character's tastes
-            foreach (int t in characterTastes[characterNum]) //step through this character's tastes and store the ID of the taste in t
-            {
-                this.AddTaste(tasteDictionary[t]); //look it up in the dictionary and add Delegate function to our list of Taste delegate functions
-				thingsILike = thingsILike + humanReadableTasteDictionary[t]; //Then add it to our text to display
-				//if the size of myTastes isn't the same as the size of the array of this character's tastes, then we
-				//haven't gotten all of them yet and therefore need an "and" in our human-readable string
-				if (myTastes.Count != characterTastes[characterNum].Length) { //If this isn't the last taste we've got
-					thingsILike = thingsILike + " and ";
-				}
-            }
-			//Let's see if all that text-making worked or not
-			Debug.Log("My character number is " + characterNum + " and I like " + thingsILike);
-		if (Application.loadedLevelName == "WordMaking"){
-            letterGenerator = GameObject.FindGameObjectWithTag("letterController");
-            letterControl = letterGenerator.GetComponent<LetterController>();
-        }
+	        if (tasteDictionary == null) //We only need (or can have, since it's static) one copy of this game-wide, so if it's been done already, don't do it again
+	        {
+	            tasteDictionary = new Dictionary<int, TasteCollection.Taste>();
+	            //Create the dictionary of taste ID's to functions
+	            tasteDictionary.Add(0, TasteCollection.threeLetters);
+	            tasteDictionary.Add(1, TasteCollection.fiveOrLonger);
+	            tasteDictionary.Add(2, TasteCollection.unCommonLetters);
+	            tasteDictionary.Add(3, TasteCollection.endsWithVowel);
+	            tasteDictionary.Add(4, TasteCollection.twoOrMoreVowels);
+	            tasteDictionary.Add(5, TasteCollection.twoOrMoreSame);
+	            tasteDictionary.Add(6, TasteCollection.startsWithVowel);
+	            tasteDictionary.Add(7, TasteCollection.startsAndEndsWithSame);
+	            tasteDictionary.Add(8, TasteCollection.fourLetters);
+	            tasteDictionary.Add(9, TasteCollection.noPreference);
+	            tasteDictionary.Add(10, TasteCollection.trashCollection);
+	        }
+			if (humanReadableTasteDictionary == null) { //We only need (or can have, since it's static) one copy of this game-wide, so if it's been done already, don't do it again
+				humanReadableTasteDictionary = new Dictionary<int, string> ();
+				//Create the dictionary of taste ID's to Human-readable text
+				humanReadableTasteDictionary.Add (0, "three letters");
+				humanReadableTasteDictionary.Add (1, "five letters or more");
+				humanReadableTasteDictionary.Add (2, "uncommon letters(F,H,V,W,Y,K,J,X,Q,Z)");
+				humanReadableTasteDictionary.Add (3, "ends in a vowel");
+				humanReadableTasteDictionary.Add (4, "more than one vowel");
+				humanReadableTasteDictionary.Add (5, "two or more of the same letter");
+				humanReadableTasteDictionary.Add (6, "starts with a vowel");
+				humanReadableTasteDictionary.Add (7, "starts and ends with same");
+				humanReadableTasteDictionary.Add (8, "four letters");
+				humanReadableTasteDictionary.Add (9, "anything");
+				humanReadableTasteDictionary.Add (10, "trash - things that aren't words");
+					}
+			myTastes = new List<TasteCollection.Taste>();
+			//We have to do this all the time now, in order to display the tastes correctly.
+			//{
+				//initialize the tastes for the characters
+	            //First make a generic list of the character taste arrays so that I can
+	            //easily access my tastes with my character number
+	            List<int[]> characterTastes = new List<int[]>();
+	            characterTastes.Add(new int[] { 10 }); //The trash character, ID 0, has one taste, taste 10
+	            characterTastes.Add(variables.TastesForCharacter1);
+	            characterTastes.Add(variables.TastesForCharacter2);
+	            characterTastes.Add(variables.TastesForCharacter3);
+	            characterTastes.Add(variables.TastesForCharacter4);
+	            characterTastes.Add(variables.TastesForCharacter5);
+	            //now we add an arbitrary number of tastes
+				//also we set up the text to be displayed for the character's tastes
+	            foreach (int t in characterTastes[characterNum]) //step through this character's tastes and store the ID of the taste in t
+	            {
+	                this.AddTaste(tasteDictionary[t]); //look it up in the dictionary and add Delegate function to our list of Taste delegate functions
+					thingsILike = thingsILike + humanReadableTasteDictionary[t]; //Then add it to our text to display
+					//if the size of myTastes isn't the same as the size of the array of this character's tastes, then we
+					//haven't gotten all of them yet and therefore need an "and" in our human-readable string
+					if (myTastes.Count != characterTastes[characterNum].Length) { //If this isn't the last taste we've got
+						thingsILike = thingsILike + " and ";
+					}
+	            }
+				//Let's see if all that text-making worked or not
+				Debug.Log("My character number is " + characterNum + " and I like " + thingsILike);
+			if (Application.loadedLevelName == "WordMaking"){
+	            letterGenerator = GameObject.FindGameObjectWithTag("letterController");
+	            letterControl = letterGenerator.GetComponent<LetterController>();
+	        }
+		}
     }
     // Update is called once per frame
     void Update()
