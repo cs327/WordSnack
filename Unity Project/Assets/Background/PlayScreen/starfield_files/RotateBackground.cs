2f544d1570f835d987a705fd1c805e39dd610d5f
OwenNeuburger <OwenNeuburger@greenwichwireless167-3.hampshire.edu>
2014-10-09 10:39:12 -0400

    added in wordmaking bg layers

---
+++ Unity Project/Assets/Background/PlayScreen/starfield_files/RotateBackground.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateBackground : MonoBehaviour {
+
+	public float rotateSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (new Vector3 (0, rotateSpeed, 0) * Time.deltaTime);
+	}
+}
