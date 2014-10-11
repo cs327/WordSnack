861c3c4fa2a485b5eea154feaa76bc06f54d89b7
Ning Xie <ningxie1991@gmail.com>
2014-10-10 21:39:16 -0400

    Updated the summary screen!

---
+++ Unity Project/Assets/GoBackToMenu.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoBackToMenu : MonoBehaviour {
+	Bounds bounds;
+	Camera camera;
+
+	// Use this for initialization
+	void Start () {
+		bounds = gameObject.GetComponent<BoxCollider> ().bounds;
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(UniversalInput.press && UniversalInput.inRect(bounds, camera)){
+			Application.LoadLevel("StartScreenTest");
+		}
+		
+	}
+}
