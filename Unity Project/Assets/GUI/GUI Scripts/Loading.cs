11d02c969a37e876b272043efb00986005688d19
Ning Xie <ningxie1991@gmail.com>
2014-10-11 01:11:06 -0400

    Moved a script into a folder and edited splash

---
+++ Unity Project/Assets/GUI/GUI Scripts/Loading.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Loading : MonoBehaviour {
+	
+	public static Loading instance;
+	public bool showLoadingScreen = true;
+	public Texture loadingScreen;
+	
+	void Awake(){
+		if(instance == null){
+			instance = this;
+		}
+		if(instance != this){
+			Destroy(gameObject);
+		}
+		else{
+			//DontDestroyOnLoad(gameObject);
+		}
+	}
+	
+	// Update is called once per frame
+	void OnGUI () {
+		if(showLoadingScreen && Application.isLoadingLevel && loadingScreen != null){
+			GUI.DrawTexture(new Rect(0,0,Screen.width,Screen.height),loadingScreen);
+		}
+	}
+}
