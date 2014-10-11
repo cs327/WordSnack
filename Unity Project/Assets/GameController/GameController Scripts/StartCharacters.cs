813d3d1cca6817fa56190345a6be87d79d80204b
imann24 <imann24@gmail.com>
2014-10-11 12:45:54 -0400

    Deleted Extraneous Files/Folders
    General Housekeeping

--- Unity Project/Assets/GameController/GameController Scripts/StartCharacters.cs
+++ Unity Project/Assets/GameController/GameController Scripts/StartCharacters.cs
@@ -5,8 +5,8 @@ public class StartCharacters : MonoBehaviour {
 	public GameObject [] characters =  new GameObject[5];
 	// Use this for initialization
 	void Start () {
-		Instantiate (characters[PlayerPrefs.GetInt("Character 1")-1], new Vector3 (-5, 0, -1), Quaternion.identity);
-		Instantiate (characters[PlayerPrefs.GetInt("Character 2")-1], new Vector3 (5, 0, -1), Quaternion.identity);
+		Instantiate (characters[PlayerPrefs.GetInt("Character 1")-1], new Vector3 (-8, 0, -1), Quaternion.identity);
+		Instantiate (characters[PlayerPrefs.GetInt("Character 2")-1], new Vector3 (1.5F, 0, -1), Quaternion.identity);
 	}
 	
 	// Update is called once per frame
