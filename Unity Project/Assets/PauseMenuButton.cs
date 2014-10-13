68896dbcdd4d6afab763b017738c44a9705360b0
Ning Xie <ningxie1991@gmail.com>
2014-10-13 16:43:41 -0400

    added pause button to the word making phase

---
+++ Unity Project/Assets/PauseMenuButton.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseMenuButton : MonoBehaviour {
+
+	public GameObject dinerRoom;
+	public GameObject pauseMenuBackground;
+	//public GameObject letterGen;
+	//public GameObject gameController;
+	Camera camera;
+	Bounds pauseButtonBounds;
+
+	// Use this for initialization
+	void Start () {
+		pauseButtonBounds = gameObject.GetComponent<BoxCollider> ().bounds;
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		pauseMenuBackground.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(UniversalInput.press && UniversalInput.inRect(pauseButtonBounds, camera)){
+			dinerRoom.SetActive(false);
+			pauseMenuBackground.SetActive(true);
+			//letterGen.SetActive(false);
+			//gameController.SetActive(false);
+			Time.timeScale = 0;
+		}else{
+
+		}
+	}
+}
