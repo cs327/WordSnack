8824ba3b27dea68044ffe475224fc3b0bf1031db
hklales <hklales@Hannah-Klaless-MacBook-Pro.local>
2014-10-13 15:48:43 -0400

    audio in every scene

---
+++ Unity Project/Assets/Audio/PlayMusic3.cs
@@ -0,0 +1,22 @@
+﻿//Hannah
+//Plays all aduio in wordmaking scene!
+using UnityEngine;
+using System.Collections;
+
+public class PlayMusic3 : MonoBehaviour {
+
+	public GameObject audio;
+	AudioManager audioManager;
+	
+	// Use this for initialization
+	void Start () {
+		audioManager = audio.GetComponent<AudioManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		audioManager.PlayLoop(9);
+		
+	}
+}
