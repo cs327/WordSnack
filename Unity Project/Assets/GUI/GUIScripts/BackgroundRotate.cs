844e0e7b9d8365bf92b246f2198143e2d8ac1e16
imann24 <imann24@gmail.com>
2014-10-14 17:11:29 -0400

    Added Dynamic Background to MenuScreen

---
+++ Unity Project/Assets/GUI/GUIScripts/BackgroundRotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundRotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		gameObject.transform.Rotate(0, -Time.deltaTime*5, 0, Space.World);
+	}
+}
