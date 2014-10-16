a4684955deb9e6a7188c3ce13c095286ec346682
Ning Xie <ningxie1991@gmail.com>
2014-10-15 21:36:49 -0400

    updated the score display in the receipt

--- Unity Project/Assets/GUI/GUIScripts/ScoreGUI.cs
+++ Unity Project/Assets/GUI/GUIScripts/ScoreGUI.cs
@@ -5,7 +5,7 @@ public class ScoreGUI : MonoBehaviour {
 
 	public GUIStyle big;
 	public int characterNum;
-	public TextMesh totalScoreText;
+	TextMesh totalScoreTextMesh;
 	public TextMesh[] wordsFed;
 	int selectedCharacter1;
 	int selectedCharacter2;
@@ -14,12 +14,19 @@ public class ScoreGUI : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		score = PlayerPrefs.GetFloat("Score");
-		totalScoreText = 
-		gameObject.GetComponent<TextMesh> ().text = score.ToString();
+		totalScoreTextMesh = transform.GetChild (0).GetComponent<TextMesh> ();
+		wordsFed = new TextMesh[characterNum];
+
+
+
 		selectedCharacter1 = PlayerPrefs.GetInt ("Character 1");
 		selectedCharacter2 = PlayerPrefs.GetInt ("Character 2");
 		Debug.Log (selectedCharacter1);
 		Debug.Log (selectedCharacter2);
+	}
+
+	void Update(){
+		totalScoreTextMesh.text = score.ToString();
 	}
 
 	//Method to display words fed
