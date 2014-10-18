34eb495c41dd5849143ac56c8c2dbb0ff3377ee4
hklales <hklales@hampguest70-146.hampshire.edu>
2014-10-18 15:27:50 -0400

    new sounds in char select

--- Unity Project/Assets/Audio/PlayMusic2.cs
+++ Unity Project/Assets/Audio/PlayMusic2.cs
@@ -4,19 +4,52 @@ using UnityEngine;
 using System.Collections;
 
 public class PlayMusic2 : MonoBehaviour {
-
-	public GameObject audio;
-	AudioManager audioManager;
-
-	// Use this for initialization
-	void Start () {
-		audioManager = audio.GetComponent<AudioManager>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-
-		audioManager.PlayLoop(8);
-	
-	}
+    
+    public GameObject audio;
+    AudioManager audioManager;
+    
+    public GameObject selecter;
+    CharacterSelectUI charSel;
+    
+    public GameObject vControl;
+    VariableControl variableControl;
+    
+    int numSelected = 0;
+    
+    // Use this for initialization
+    void Start () {
+        audioManager = audio.GetComponent<AudioManager>();
+        charSel = selecter.GetComponent<CharacterSelectUI>();
+        variableControl = vControl.GetComponent<VariableControl>();
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        
+        audioManager.PlayLoop(8);
+        
+        if (variableControl.currentCharacterSelectNum == 0)
+        {
+            numSelected = 0;
+            
+        }
+        
+        if (variableControl.currentCharacterSelectNum>numSelected)
+        {
+            audioManager.Play(1);
+            numSelected ++;
+        }
+        
+        if (variableControl.currentCharacterSelectNum<numSelected)
+        {
+            audioManager.Play(1);
+            numSelected --;
+        }
+        
+        
+        if (charSel.FeedPressed == true)
+        {
+            audioManager.Play(1);
+        }
+    }
 }
