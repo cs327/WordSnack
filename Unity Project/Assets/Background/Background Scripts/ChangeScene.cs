ca769ada24cb649a678cf3e9d9d7eab27a4fe708
deanMike <msd14@hampshire.edu>
2014-10-22 21:26:15 -0400

    Revert "Revert "Revert "More Housekeeping"""
    This reverts commit 7139316100a5a89cf8b2149f250c3a75912fcd18.

---
+++ Unity Project/Assets/Background/Background Scripts/ChangeScene.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using System.Collections;
+
+public class ChangeScene : MonoBehaviour {
+
+	public string versionNum = "version 0.0.6";
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Time.time > 3) {
+			if (Application.loadedLevelName == "SplashScreen") {
+				Application.LoadLevel("StartScreenTest");
+			}
+		}
+	
+	}
+
+    
+	void OnMouseDown () {
+		if (Application.loadedLevelName == "SplashScreen") {
+			Application.LoadLevel("StartScreenTest");
+		}
+	}
+
+	void OnGUI(){
+		GUIStyle style = new GUIStyle ();
+		style.fontSize = 22;
+		style.normal.textColor = Color.white;
+		GUI.Label (new Rect (Screen.width * 0.7f, Screen.height * 0.05f, Screen.width * 0.3f, Screen.height * 0.12f), versionNum, style);
+	}
+}
