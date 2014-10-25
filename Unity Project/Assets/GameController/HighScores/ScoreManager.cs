06631c351a99b2f649fc471d174cfee1b332ba6a
David Carlson <britishorbust@gmail.com>
2014-10-25 15:12:55 -0400

    Modified how high scores work to include the characters played

--- Unity Project/Assets/GameController/HighScores/ScoreManager.cs
+++ Unity Project/Assets/GameController/HighScores/ScoreManager.cs
@@ -5,10 +5,12 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System;
+using System.Linq;
 
 public static class ScoreManager 
 {
-    public static List<int> scoreList;
+    public static List<string> scoreList;
     
     // Opens the save file, saves the current scoreList, then closes
     private static void SaveScores()
@@ -20,23 +22,40 @@ public static class ScoreManager
         file.Close();
     }
 
+    // Give it two characters (i.e "Spike" and "Fred") and it will return
+    // a sorted list of high scores
+    public static List<string> GetScoresForSpecificCharacters(string char1, string char2)
+    {
+        string[] characters = {char1, char2};
+        LoadScores();
+
+        // Filter the list such that each score is only for the two given characters
+        var tempList = scoreList.Where<string>((x) => characters.Contains<string>(x.Split(' ')[0]) && characters.Contains<string>(x.Split()[1]));
+
+        return tempList.ToList<string>();
+    }
+
     // Given a score, this loads the score list, adds it to the list,
     // saves the list back to file and the returns the score list
-    public static List<int> AddHighScore(int score)
+    public static List<string> AddHighScore(string char1, string char2, int score)
     {
         int scoreListSizeLimit = GameObject.Find("GameController").GetComponent<VariableControl>().scoreListSize;
 
         if (scoreList == null)
             LoadScores();
-       
-        scoreList.Add(score);
+
+        scoreList.Add(String.Format("{0] {1} {2}", char1, char2, score.ToString()));
         if (scoreList.Count > scoreListSizeLimit)
-        {
-            scoreList.Sort();
+        {            
             scoreList.RemoveRange(scoreListSizeLimit, scoreList.Count - scoreListSizeLimit);
         }
         SaveScores();
         return scoreList;
+    }
+
+    private static void sortByScore(List<string> scores)
+    {
+        scores.Sort((x, y) => (Convert.ToInt32(x.Split(' ')[2]) >= Convert.ToInt32(y.Split(' ')[2])) ? 1 : -1);    
     }
 
     // Reads the save file if it exists, loads the scores into scoreList
@@ -47,13 +66,13 @@ public static class ScoreManager
         {
             BinaryFormatter bf = new BinaryFormatter();
             FileStream file = File.Open(Application.persistentDataPath + "/savedGames.gd", FileMode.Open);
-            scoreList = (List<int>) bf.Deserialize(file);
+            scoreList = (List<string>) bf.Deserialize(file);
             scoreList.Sort();
             file.Close();
         }
         else
         {
-            scoreList = new List<int>();
+            scoreList = new List<string>();
         }
     }
 
