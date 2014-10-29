a17414e20b9284c2191c782b683ee59c26f0f475
Wm. Josiah Erikson <josiah@insanetechnology.com>
2014-10-29 18:29:03 -0400

    Commented out the instructions Debug line

--- Unity Project/Assets/GameController/GameController Scripts/wordBuildingController.cs
+++ Unity Project/Assets/GameController/GameController Scripts/wordBuildingController.cs
@@ -1,79 +1,83 @@
 using UnityEngine;
 using System.Collections;
 
-public class wordBuildingController : MonoBehaviour {
-	public GameObject [] characters = new GameObject[6];
-	public VariableControl variables;
-	public GameObject variableController;
-	public Texture2D endGameButton;
-	int character1Num = 1;
-	int character2Num = 2;
-	public GameObject character1;
-	public GameObject character2;
-	public GameObject trashCharacter;
-	public bool gamePaused;
-	public GameObject tutorial;
-	//public GameObject instructionsClose;
-	public TextMesh lettersRemaining;
-	// Use this for initialization
-	void Start () {
-		variables = variableController.GetComponent<VariableControl>();
-		character1Num = PlayerPrefs.GetInt("Character 1");
-		character2Num = PlayerPrefs.GetInt("Character 2");
-		character1 = (GameObject)Instantiate(characters[character1Num], variables.phase2CharacterPositions[0], Quaternion.identity);
-		character2 = (GameObject)Instantiate(characters[character2Num], variables.phase2CharacterPositions[1], Quaternion.identity);
-		character1.transform.localScale = new Vector3 (2,2,2);
-		character2.transform.localScale = new Vector3 (2,2,2);
-		//Set the characterNums correctly
-		character1.GetComponent<Character> ().characterNum = character1Num;
-		character2.GetComponent<Character> ().characterNum = character2Num;
+public class wordBuildingController : MonoBehaviour
+{
+		public GameObject[] characters = new GameObject[6];
+		public VariableControl variables;
+		public GameObject variableController;
+		public Texture2D endGameButton;
+		int character1Num = 1;
+		int character2Num = 2;
+		public GameObject character1;
+		public GameObject character2;
+		public GameObject trashCharacter;
+		public bool gamePaused;
+		public GameObject tutorial;
+		//public GameObject instructionsClose;
+		public TextMesh lettersRemaining;
+		// Use this for initialization
+		void Start ()
+		{
+				variables = variableController.GetComponent<VariableControl> ();
+				character1Num = PlayerPrefs.GetInt ("Character 1");
+				character2Num = PlayerPrefs.GetInt ("Character 2");
+				character1 = (GameObject)Instantiate (characters [character1Num], variables.phase2CharacterPositions [0], Quaternion.identity);
+				character2 = (GameObject)Instantiate (characters [character2Num], variables.phase2CharacterPositions [1], Quaternion.identity);
+				character1.transform.localScale = new Vector3 (2, 2, 2);
+				character2.transform.localScale = new Vector3 (2, 2, 2);
+				//Set the characterNums correctly
+				character1.GetComponent<Character> ().characterNum = character1Num;
+				character2.GetComponent<Character> ().characterNum = character2Num;
 
-		PlayerPrefs.SetInt("Instructions",0);
-		tutorial = GameObject.Find ("Tutorial");
-		//instructionsClose = GameObject.Find ("close");
+				PlayerPrefs.SetInt ("Instructions", 0);
+				tutorial = GameObject.Find ("Tutorial");
+				//instructionsClose = GameObject.Find ("close");
 
-        if (GameObject.Find("AudioManager_Prefab(Clone)") == null)
-        {
-            Instantiate(Resources.Load("AudioManager_Prefab"), new Vector3(0, 0, 0), Quaternion.identity);
-        }
-	}
+				if (GameObject.Find ("AudioManager_Prefab(Clone)") == null) {
+						Instantiate (Resources.Load ("AudioManager_Prefab"), new Vector3 (0, 0, 0), Quaternion.identity);
+				}
+		}
 	
-	// Update is called once per frame
-	void Update () {
-		lettersRemaining.text = variables.lettersRemaining.ToString();
-		Debug.Log ("playerprefs instructions: " + PlayerPrefs.GetInt("instructions"));
-		// if the instructions is enabled 
-		if(PlayerPrefs.GetInt("instructions") == 0){
-			tutorial.SetActive (true);
-			//instructionsClose.SetActive(true);
-		}else{
-			tutorial.SetActive (false);
-			//instructionsClose.SetActive(false);
+		// Update is called once per frame
+		void Update ()
+		{
+				lettersRemaining.text = variables.lettersRemaining.ToString ();
+				//Debug.Log ("playerprefs instructions: " + PlayerPrefs.GetInt("instructions"));
+				// if the instructions is enabled 
+				if (PlayerPrefs.GetInt ("instructions") == 0) {
+						tutorial.SetActive (true);
+						//instructionsClose.SetActive(true);
+				} else {
+						tutorial.SetActive (false);
+						//instructionsClose.SetActive(false);
+				}
 		}
-	}
 
-	void OnGUI () {
-		if(!gamePaused){
-			GUIStyle style = new GUIStyle ();
-			style.normal.background = endGameButton;
-			if (GUI.Button (new Rect(Screen.width*0.013f, Screen.height*0.315f, Screen.width*0.07f, Screen.width*0.07f), "", style)) {
-				sendVariablestoScoreScreen();
-				Application.LoadLevel("ScoreScreen");
-			}
+		void OnGUI ()
+		{
+				if (!gamePaused) {
+						GUIStyle style = new GUIStyle ();
+						style.normal.background = endGameButton;
+						if (GUI.Button (new Rect (Screen.width * 0.013f, Screen.height * 0.315f, Screen.width * 0.07f, Screen.width * 0.07f), "", style)) {
+								sendVariablestoScoreScreen ();
+								Application.LoadLevel ("ScoreScreen");
+						}
+				}
 		}
-	}
-	public void sendVariablestoScoreScreen () {
-		//updates score related variables 
-		variables.totalLetterScore = character1.GetComponent<Character>().rawScoreFedToMe + character2.GetComponent<Character>().rawScoreFedToMe + trashCharacter.GetComponent<Character>().rawScoreFedToMe;
-		variables.totalMultiplierScore = character1.GetComponent<Character>().rawBonusScoreFedToMe + character2.GetComponent<Character>().rawBonusScoreFedToMe + trashCharacter.GetComponent<Character>().rawBonusScoreFedToMe;
-		variables.trashedLetters = trashCharacter.GetComponent<Character>().numLettersFedToMe;
-		variables.trashedLetterScore = trashCharacter.GetComponent<Character>().rawScoreFedToMe;
+		public void sendVariablestoScoreScreen ()
+		{
+				//updates score related variables 
+				variables.totalLetterScore = character1.GetComponent<Character> ().rawScoreFedToMe + character2.GetComponent<Character> ().rawScoreFedToMe + trashCharacter.GetComponent<Character> ().rawScoreFedToMe;
+				variables.totalMultiplierScore = character1.GetComponent<Character> ().rawBonusScoreFedToMe + character2.GetComponent<Character> ().rawBonusScoreFedToMe + trashCharacter.GetComponent<Character> ().rawBonusScoreFedToMe;
+				variables.trashedLetters = trashCharacter.GetComponent<Character> ().numLettersFedToMe;
+				variables.trashedLetterScore = trashCharacter.GetComponent<Character> ().rawScoreFedToMe;
 
-		//saves those variables in Player Prefs
-		PlayerPrefs.SetFloat ("Score", variables.score);
-		PlayerPrefs.SetInt( "Total Letter Score" , variables.totalLetterScore ); 
-		PlayerPrefs.SetInt( "Total Multiplier Score" , variables.totalMultiplierScore ); 
-		PlayerPrefs.SetInt( "Trashed Letters" , variables.trashedLetters ); 
-		PlayerPrefs.SetInt( "Trashed Letter Score" , variables.trashedLetterScore ); 
-	}
+				//saves those variables in Player Prefs
+				PlayerPrefs.SetFloat ("Score", variables.score);
+				PlayerPrefs.SetInt ("Total Letter Score", variables.totalLetterScore); 
+				PlayerPrefs.SetInt ("Total Multiplier Score", variables.totalMultiplierScore); 
+				PlayerPrefs.SetInt ("Trashed Letters", variables.trashedLetters); 
+				PlayerPrefs.SetInt ("Trashed Letter Score", variables.trashedLetterScore); 
+		}
 }
