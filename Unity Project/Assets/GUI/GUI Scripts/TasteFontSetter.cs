c23bb866f0d2fd5f2785097eaed2dd26caae34b3
imann24 <imann24@gmail.com>
2014-10-30 21:10:56 -0400

    Added 2D Art Approved Fonts and Font Colors

---
+++ Unity Project/Assets/GUI/GUI Scripts/TasteFontSetter.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TasteFontSetter : MonoBehaviour {
+	public Font wordMakingFont;
+	public Font fontForCharacterSelect;
+	// Use this for initialization
+	void Start () {
+		if (Application.loadedLevelName == "CharacterSelectTest") {
+			gameObject.GetComponent<TextMesh>().font = fontForCharacterSelect;
+		} else if (Application.loadedLevelName == "WordMaking") {
+			gameObject.GetComponent<TextMesh>().font = wordMakingFont;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
