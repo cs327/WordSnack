fe6584d76a045264bdc331834b14034ea654edb6
Owen <otn12@hampshire.edu>
2014-11-07 18:01:04 -0500

    commented pause script

--- Unity Project/Assets/Background/PlayScreen/diner2/pause.cs
+++ Unity Project/Assets/Background/PlayScreen/diner2/pause.cs
@@ -12,7 +12,11 @@ public class pause : MonoBehaviour {
 	public GameObject greyBg;
 	public bool unpause = false;
 
-
+//notes on this script:
+//the pause function is called by clicking on the button,
+//however the unpause function is called in a tiny script on the resume button,
+//so if looking for triggering the unpause, look at resumeButton.cs, not this script
+//this script only has basically everything else related to flipping the paused bool in variable control.
 
 	// Use this for initialization
 	void Start () {
@@ -21,15 +25,19 @@ public class pause : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		//if the unpause bool is true, unpause the game
 		if(unpause){
 			unpauseGame();
 		}
 	}
 
 	void OnMouseDown(){
+		//when clicked down on, change texture
 		gameObject.renderer.material.SetTexture("_MainTex", pauseButtons[1]);
 	}
 	void OnMouseUp(){
+		//when unclicked- change texture back, and if the game is not paused (which
+		//it probably isnt), pause it
 		gameObject.renderer.material.SetTexture("_MainTex", pauseButtons[0]);
 		if(!variables.paused){
 			pauseGame();
@@ -38,6 +46,8 @@ public class pause : MonoBehaviour {
 	}
 
 	void pauseGame(){
+		//set the global paused variable to true and activate the objects that 
+		//constitute the pause menu
 		variables.paused = true;
 		greyBg.SetActive(true);
 		resumeButtonObj.SetActive(true);
@@ -45,6 +55,8 @@ public class pause : MonoBehaviour {
 	}
 
 	void unpauseGame(){
+		//set the unpause bool to false (since the game is about to be unpaused and then will no
+		//longer need it), set the global pause to off, and deactivate all objects associated with pause menu
 		unpause = false;
 		variables.paused = false;
 		greyBg.SetActive(false);
