9f0f2a5d4441925226ca766b1679715e80af0f86
David Carlson <britishorbust@gmail.com>
2014-11-07 00:03:29 -0500

    Changed the instruction screen setup and changed out the 'never show' works

--- Unity Project/Assets/GUI/GUIScripts/Tutorial.cs
+++ Unity Project/Assets/GUI/GUIScripts/Tutorial.cs
@@ -19,7 +19,6 @@ public class Tutorial : MonoBehaviour
     wordBuildingController w;
     GameObject character1;
     GameObject character2;
-    public GameObject checkMark;
     public bool clickSound;
 
     // Use this for initialization
@@ -49,8 +48,9 @@ public class Tutorial : MonoBehaviour
         //checkBox = new Rect (Screen.width * 0.32f, Screen.height * 0.54f, Screen.width * 0.04f, Screen.height * 0.055f);
         w = GameObject.Find("GameController").GetComponent<wordBuildingController>();
 
-        checkMark.renderer.enabled = false;
-        checkMark.collider.enabled = false;
+        trash.position = new Vector3(trash.position.x, trash.position.y, -2.12f);
+        transform.localPosition = new Vector3(0.06f, 0.429f, -0.86f);
+        transform.localScale = new Vector3(0.73f, 1.21f, 0.82f);
     }
 
     void Update()
@@ -73,6 +73,16 @@ public class Tutorial : MonoBehaviour
         }
     }
 
+    // Saves the current value of "Never show instructions?" in the file UserSettings.gd
+    private static void SaveNeverShowSetting(bool setting)
+    {
+        Debug.Log("Saving 'never show? = " + setting + "'");
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Create(Application.persistentDataPath + "/UserSettings.gd");
+        bf.Serialize(file, setting);
+        file.Close();
+    }
+
     // Update is called once per frame
     void OnMouseDown()
     {
@@ -91,77 +101,16 @@ public class Tutorial : MonoBehaviour
         if (m.renderer.material.mainTexture == instructions[0])
         {
             m.renderer.material.mainTexture = instructions[1];
-            transform.localPosition = new Vector3(-1.0855f, .429f, .1266f);
-            transform.localScale = new Vector3(0.55f, 1.21f, 0.53f);
-            gameObject.GetComponent<BoxCollider>().center = new Vector3(1.87f, 0, -0.16f);
-            trash.position = new Vector3(trash.position.x, trash.position.y, -2.12f);
-        }
-        else if (m.renderer.material.mainTexture == instructions[1])
-        {            
-            m.renderer.material.mainTexture = instructions[2];
-            transform.localPosition = new Vector3(-0.06f, 0.429f, 1.0888f);
-            transform.localScale = new Vector3(0.67f, 1.21f, 0.59f);
-            trash.position = new Vector3(trash.position.x, trash.position.y, -1.12f);
-            gameObject.GetComponent<BoxCollider>().size = new Vector3(gameObject.GetComponent<BoxCollider>().size.x, gameObject.GetComponent<BoxCollider>().size.y, 27.59f);
-            gameObject.GetComponent<BoxCollider>().center = new Vector3(-0.04f, 0, -4.4f);
-            character1.transform.localPosition = new Vector3(character1.transform.localPosition.x, character1.transform.localPosition.y, -1.8f);
-            character2.transform.localPosition = new Vector3(character2.transform.localPosition.x, character2.transform.localPosition.y, -1.8f);
-
-        }
-        else if (m.renderer.material.mainTexture == instructions[2])
-        {
-            m.renderer.material.mainTexture = instructions[3];           
             transform.localPosition = new Vector3(0.06f, 0.429f, -0.86f);
             transform.localScale = new Vector3(0.73f, 1.21f, 0.82f);
             character1.transform.localPosition = new Vector3(character1.transform.localPosition.x, character1.transform.localPosition.y, 1);
-            character2.transform.localPosition = new Vector3(character2.transform.localPosition.x, character2.transform.localPosition.y, 1);
-        }
-        else if (m.renderer.material.mainTexture == instructions[3])
+            character2.transform.localPosition = new Vector3(character2.transform.localPosition.x, character2.transform.localPosition.y, 1);           
+        }      
+        else if (m.renderer.material.mainTexture == instructions[1])
         {
-            Debug.Log("reached the last instructions");
-            m.renderer.material.mainTexture = instructions[4];
-            transform.localPosition = new Vector3(0, 0.429f, -1.04f);
-            transform.localScale = new Vector3(0.64f, 1.21f, 0.52f);
-            checkMark.renderer.enabled = true;
-            checkMark.GetComponent<BoxCollider>().enabled = true;
-
-        }
-        else if (m.renderer.material.mainTexture == instructions[4])
-        {
-            Debug.Log("clicked on the last instructions");
-            if (Input.touchCount > 0)
-            {
-                // for touch
-                if (checkMark.collider.bounds.Contains(new Vector2(Input.GetTouch(0).position.x, Screen.height - Input.GetTouch(0).position.y)))
-                {
-                    Debug.Log("clicked on the check box");
-
-                }
-                else
-                {
-
-                    parentPos.x = -20.0f;
-                    transform.parent.transform.position = parentPos;
-                    //					PlayerPrefs.SetInt("instructions",0);
-                }
-            }
-            else
-            {
-                // for mouse clicks
-                if (checkMark.collider.bounds.Contains(new Vector2(Input.mousePosition.x, Screen.height - Input.mousePosition.y)))
-                {
-                    Debug.Log("clicked on the check box");
-                    checkMark.renderer.enabled = !checkMark.renderer.enabled;
-                    
-                }
-                else
-                {
-                    Debug.Log("clicked on the screen but not the check box");
-                    parentPos.x = -20.0f;
-                    transform.parent.transform.position = parentPos;
-                    
-                }
-            }
+            SaveNeverShowSetting(true);
+            parentPos.x = -20.0f;
+            transform.parent.transform.position = parentPos;
         }
     }
 }
