4c5138989062d8dcd5fadac08d515ca989641a93
hklales <hklales@Hannah-Klaless-MacBook-Pro.local>
2014-11-16 18:37:48 -0500

    more cool changes

--- Unity Project/Assets/GameController/GameController Scripts/wordBuildingController.cs
+++ Unity Project/Assets/GameController/GameController Scripts/wordBuildingController.cs
@@ -52,13 +52,13 @@ public class wordBuildingController : MonoBehaviour
             
 
 				tasteHighlighters [0] = char1Taste1;
-				tasteHighLightPos [0] = new Vector3 (-7.6f, 4.6f, -0.5f);
+                tasteHighLightPos [0] = new Vector3(0.0f, 4.82f, 0.08f);         // (-7.6f, 4.6f, -0.5f);
 				tasteHighlighters [1] = char1Taste2;
-				tasteHighLightPos [1] = new Vector3 (-7.6f, 3.9f, -0.5f);
+        tasteHighLightPos [1] = new Vector3 (0.0f, 4.82f, 0.08f);          //(-7.6f, 3.9f, -0.5f);
 				tasteHighlighters [2] = char2Taste1;
-				tasteHighLightPos [2] = new Vector3 (7.6f, 4.6f, -0.5f);
+        tasteHighLightPos [2] = new Vector3 (0.0f, 4.82f, 0.08f); //(7.6f, 4.6f, -0.5f);
 				tasteHighlighters [3] = char2Taste2;
-				tasteHighLightPos [3] = new Vector3 (7.6f, 3.9f, -0.5f);
+        tasteHighLightPos [3] = new Vector3 (0.0f, 4.82f, 0.08f); //(7.6f, 3.9f, -0.5f);
 				character1Num = PlayerPrefs.GetInt ("Character 1");
 				character2Num = PlayerPrefs.GetInt ("Character 2");
 				character1 = (GameObject)Instantiate (characters [character1Num], variables.phase2CharacterPositions [0], Quaternion.identity);
