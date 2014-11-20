9a99f5fb841e9efae0c4453c18cd4cc06483470e
David Carlson <britishorbust@gmail.com>
2014-11-19 22:21:21 -0500

    Hopefully fixed high scores

--- Unity Project/Assets/GameController/HighScores/ScoreManager.cs
+++ Unity Project/Assets/GameController/HighScores/ScoreManager.cs
@@ -1,73 +1,61 @@
-﻿using System.Collections.Generic;
-//using System.Collections.Specialized;
-using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
-using System.Collections;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
-using System.Linq;
-using System;
 
 public class ScoreManager
 {
-		void Awake ()
-		{
-				// Forces a different code path in the BinaryFormatter that doesn't rely on run-time code generation (which would break on iOS).
-				//Environment.SetEnvironmentVariable ("MONO_REFLECTION_SERIALIZER", "yes");
-				//Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes");
-		}
-		// Stores a description of a high score and a list of scores that fit the description. 
-		// i.e "Timed Meghan Stella" might return [323, 200, 100], those being the top three scores by
-		// Meghan and Stella on timed mode
-		private static Dictionary<string, List<int>> scoreList = new Dictionary<string, List<int>> ();
-		public static bool NeverShowInstructions = false;
+    void Awake()
+    {
+        // Forces a different code path in the BinaryFormatter that doesn't rely on run-time code generation (which would break on iOS).
+        //Environment.SetEnvironmentVariable ("MONO_REFLECTION_SERIALIZER", "yes");
+        //Environment.SetEnvironmentVariable("MONO_REFLECTION_SERIALIZER", "yes");
+    }
+    // Stores a description of a high score and a list of scores that fit the description. 
+    // i.e "Timed Meghan Stella" might return [323, 200, 100], those being the top three scores by
+    // Meghan and Stella on timed mode
+    private static Dictionary<string, List<int>> scoreList = new Dictionary<string, List<int>>();
+    public static bool NeverShowInstructions = false;
 
-		// Opens the save file, saves the current scoreList, then closes
-		private static void SaveScores ()
-		{
-				BinaryFormatter bf = new BinaryFormatter ();
-				FileStream file = File.Create (Application.persistentDataPath + @"/HighScoreSaves.gd");
-				bf.Serialize (file, scoreList);
-				file.Close ();
+    // Opens the save file, saves the current scoreList, then closes
+    private static void SaveScores()
+    {
+        //BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Create(Application.persistentDataPath + @"/HighScoreSaves.gd");
+        //bf.Serialize(file, scoreList);
+        //file.Close();
 
-				Debug.Log ("Saving highscores to " + Application.persistentDataPath);
-		}
+        //Debug.Log("Saving highscores to " + Application.persistentDataPath);
 
-		public static void ClearHighScores ()
-		{
-				if (File.Exists (Application.persistentDataPath + @"/HighScoreSaves.gd")) {
-						File.Delete (Application.persistentDataPath + @"/HighScoreSaves.gd");
-				}
-				scoreList = new Dictionary<string, List<int>> ();
-		}
+        PlayerPrefs.SetString("HighScores", ObjectToStr<Dictionary<string, List<int>>>(scoreList));
+    }
 
-		// Given two characters, returns the sorted score list or null if they don't exist
-		public static List<int> GetAllCharacterScore (string char1, string char2)
-		{
-				string gameMode = PlayerPrefs.GetInt ("timed") == 1 ? "timed" : "casual";
-				string charKey = gameMode + " " + GetCharacterHash (char1, char2);
+    public static void ClearHighScores()
+    {
+        //if (File.Exists(Application.persistentDataPath + @"/HighScoreSaves.gd"))
+        //{
+        //    File.Delete(Application.persistentDataPath + @"/HighScoreSaves.gd");
+        //}
+        //scoreList = new Dictionary<string, List<int>>();
+    }
 
-				if (scoreList.ContainsKey (charKey))
-						return scoreList [charKey];
-				else
-						return new List<int> ();
-		}
+    // Given two characters, returns the sorted score list or null if they don't exist
+    public static List<int> GetAllCharacterScore(string char1, string char2)
+    {
+        string gameMode = PlayerPrefs.GetInt("timed") == 1 ? "timed" : "casual";
+        string charKey = gameMode + " " + GetCharacterHash(char1, char2);
 
-		// Saves the current value of "Never show instructions?" in the file UserSettings.gd
-		private static void SaveNeverShowSetting (bool setting)
-		{
-				Debug.Log ("Saving 'never show? = " + setting + "'");
-				BinaryFormatter bf = new BinaryFormatter ();
-				FileStream file = File.Create (Application.persistentDataPath + "/UserSettings.gd");
-				bf.Serialize (file, setting);
-				file.Close ();
-		}
+        if (scoreList.ContainsKey(charKey))
+            return scoreList[charKey];
+        else
+            return new List<int>();
+    }
 
-		// Given two characters, this returns their highest score for the
-		// game mode currently set
-		public static string GetTopScore (string char1, string char2)
-		{
-				/*
+    // Given two characters, this returns their highest score for the
+    // game mode currently set
+    public static string GetTopScore(string char1, string char2)
+    {
         LoadScores();
         string gameMode = PlayerPrefs.GetInt("timed") == 1 ? "timed" : "casual";
         string charKey = gameMode + " " + GetCharacterHash(char1, char2);
@@ -80,28 +68,27 @@ public class ScoreManager
         {
             return GetAllCharacterScore(char1, char2)[0].ToString();
         }
-        */
-				return "10";
-		}
+    }
 
-		// Given two character names returns a unique value representing their set
-		private static string GetCharacterHash (string char1, string char2)
-		{
-				if (char1.CompareTo (char2) < 0)
-						return char1 + " " + char2 + " ";
-				if (char1.CompareTo (char2) > 0)
-						return char2 + " " + char1 + " ";
-				else {
-						Debug.Log (char1 + " hashed to " + char2);
-						return char1 + " " + char2 + " ";
-				}
-		}
+    // Given two character names returns a unique value representing their set
+    private static string GetCharacterHash(string char1, string char2)
+    {
+        if (char1.CompareTo(char2) < 0)
+            return char1 + " " + char2 + " ";
+        if (char1.CompareTo(char2) > 0)
+            return char2 + " " + char1 + " ";
+        else
+        {
+            Debug.Log(char1 + " hashed to " + char2);
+            return char1 + " " + char2 + " ";
+        }
+    }
 
-		// Given a score and the players used to get the score this method
-		// adds it to a lookup table and returns true if it's a high score for those characters
-		public static bool AddHighScore (string gameMode, string char1, string char2, int score)
-		{
-				/*
+    // Given a score and the players used to get the score this method
+    // adds it to a lookup table and returns true if it's a high score for those characters
+    public static bool AddHighScore(string gameMode, string char1, string char2, int score)
+    {
+
         //int scoreListSizeLimit = GameObject.Find("GameController").GetComponent<VariableControl>().scoreListSize;
         int scoreListSizeLimit = 10;
 
@@ -130,37 +117,94 @@ public class ScoreManager
 
         // If the current score is the highest score, return true
         return scoreList[charKey][0] == score ? true : false;
-        */
-				return true;
-		}
+    }
 
-		// Reads the save file if it exists, loads the scores into scoreList
-		private static void LoadScores ()
-		{
-				if (File.Exists (Application.persistentDataPath + @"/HighScoreSaves.gd")) {
-						BinaryFormatter bf = new BinaryFormatter ();
-						FileStream file = File.Open (Application.persistentDataPath + @"/HighScoreSaves.gd", FileMode.Open);
-						scoreList = (Dictionary<string, List<int>>)bf.Deserialize (file);
-						file.Close ();
-				} else {
-						Debug.Log ("HighScoresSaves not found");
-						scoreList = new Dictionary<string, List<int>> ();
-				}
-		}
+    // Saves the current value of "Never show instructions?" in the file UserSettings.gd
+    private static void SaveNeverShowSetting(bool setting)
+    {
+        //Debug.Log("Saving 'never show? = " + setting + "'");
+        //BinaryFormatter bf = new BinaryFormatter();
+        //FileStream file = File.Create(Application.persistentDataPath + "/UserSettings.gd");
+        //bf.Serialize(file, setting);
+        //file.Close();
+    }
 
-		public static string ToString ()
-		{
-				LoadScores ();
-				string str = "";
-				foreach (var key in scoreList.Keys) {
-						str += key + "have scores of ";
-						foreach (int i in scoreList[key]) {
-								str += i + ", ";
-						}
-						str += "\n";
-				}
-				return str;
-		}
+    // Reads the save file if it exists, loads the scores into scoreList
+    private static void LoadScores()
+    {
+        //if (File.Exists(Application.persistentDataPath + @"/HighScoreSaves.gd"))
+        //{
+        //    BinaryFormatter bf = new BinaryFormatter();
+        //    FileStream file = File.Open(Application.persistentDataPath + @"/HighScoreSaves.gd", FileMode.Open);
+        //    scoreList = (Dictionary<string, List<int>>)bf.Deserialize(file);
+        //    file.Close();
+        //}
+        //else
+        //{
+        //    Debug.Log("HighScoresSaves not found");
+        //    scoreList = new Dictionary<string, List<int>>();
+        //}
+
+        string scoreString = PlayerPrefs.GetString("HighScores");
+        if (scoreString == "")
+            scoreList = new Dictionary<string, List<int>>();
+        else
+            scoreList = StrToObject<Dictionary<string, List<int>>>(scoreString);
+
+        Debug.Log("ScoreList is " + scoreList.Count + " long");
+    }
+
+    public static string ToString()
+    {
+        LoadScores();
+        string str = "";
+        foreach (var key in scoreList.Keys)
+        {
+            str += key + "have scores of ";
+            foreach (int i in scoreList[key])
+            {
+                str += i + ", ";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+
+
+    public static T StrToObject<T>(string _data) where T : class
+    {
+        if (!String.IsNullOrEmpty(_data))
+        {
+            BinaryFormatter _bin = new BinaryFormatter();
+            try
+            {
+                MemoryStream _mem = new MemoryStream(Convert.FromBase64String(_data));
+
+                T _obj = _bin.Deserialize(_mem) as T;
+
+                return _obj;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+
+        }
+        else
+        {
+            throw new Exception("_data is null or empty");
+        }
+    }
+    public static string ObjectToStr<T>(T _saveMe)
+    {
+        BinaryFormatter _bin = new BinaryFormatter();
+        MemoryStream _mem = new MemoryStream();
+        _bin.Serialize(_mem, _saveMe);
+
+        return Convert.ToBase64String(
+            _mem.GetBuffer()
+        );
+    }
 
 
 
