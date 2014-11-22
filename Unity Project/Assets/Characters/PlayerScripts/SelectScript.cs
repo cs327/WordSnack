586fd8e2aff6ef8db198c33b1502561df2bd05b8
imann24 <imann24@gmail.com>
2014-11-22 15:39:12 -0500

    Fixed Weird GitHub Merge Errors

--- Unity Project/Assets/Characters/PlayerScripts/SelectScript.cs
+++ Unity Project/Assets/Characters/PlayerScripts/SelectScript.cs
@@ -12,65 +12,65 @@ public class SelectScript : MonoBehaviour {
 	//click control variables 
 	public bool selected = false;
 	public int selectNum = -1;
-
+	
 	//remember initial position
 	public Vector3 startingSpot;
-
+	
 	//Character Card images
 	public Sprite selectedImage;
 	public Sprite standbyImage;
 	public SpriteRenderer thisSprite;
-    public TextMesh HighScore;
-
+	public TextMesh HighScore;
+	
 	// variables added for displaying new UI - Ning
 	SpriteRenderer chooseTwoSprite;
 	SpriteRenderer chooseOneMoreSprite;
 	// A reference to the 
-
+	
 	public bool newSelect;
 	Camera camera;
 	Bounds characterBounds;
 	//-----------------------------------
-
+	
 	// Use this for initialization
 	void Start () {
 		//start the character with the correct sprite image (standby)
 		thisSprite = gameObject.GetComponent<SpriteRenderer>();
 		thisSprite.sprite = standbyImage;
-
+		
 		//gives the character a memory of its initial position
 		startingSpot = gameObject.transform.position;
-
+		
 		//establishes script reference
 		variables = variableController.GetComponent<VariableControl>();
 		character = gameObject.GetComponent<Character>();
 		camera = GameObject.Find ("Main Camera").GetComponent<Camera>();
 		characterBounds = gameObject.GetComponent<BoxCollider> ().bounds;
 	}
-		// Use this for initialization
-		// Update is called once per frame
-		void Update ()
-		{
-			if (variables.timeToChangeGameState && !selected) {
-				gameObject.transform.renderer.enabled = false;
-			} else {
-				gameObject.transform.renderer.enabled = true;
-			}
-			//deselects character when "Phase2" is loaded 
-			if (Application.loadedLevelName == "WordMaking" && selected) {
-				selected = false;
-				gameObject.transform.localScale = new Vector3 (0.5F, 0.5F, 0.5F);
-			}
+	// Use this for initialization
+	// Update is called once per frame
+	void Update ()
+	{
+		if (variables.timeToChangeGameState && !selected) {
+			gameObject.transform.renderer.enabled = false;
+		} else {
+			gameObject.transform.renderer.enabled = true;
 		}
-		
-
+		//deselects character when "Phase2" is loaded 
+		if (Application.loadedLevelName == "WordMaking" && selected) {
+			selected = false;
+			gameObject.transform.localScale = new Vector3 (0.5F, 0.5F, 0.5F);
+		}
+	}
 	
-
+	
+	
+	
 	void OnMouseUpAsButton () {
 		//change it to be selected or deselected based on what it already was
 		toggleSelect(); 
-
-
+		
+		
 		//only run the following during the selection phase
 		if (Application.loadedLevelName == "CharacterSelectTest"){
 			string gameMode = PlayerPrefs.GetInt("timed") == 1 ? "timed" : "casual";
@@ -93,16 +93,16 @@ public class SelectScript : MonoBehaviour {
 					variables.selectedCharacterNums[1] = character.characterNum;
 					character.charSelectOrder = 1;
 					thisSprite.sprite = selectedImage;
-
+					
 					//if a player tries to select a character but there are already 2 characters selected, it toggles the select again
-
-
+					
+					
 				}
 				//makes the sprite renderer show the "selected" card and gives it the correct transform
-
+				
 				gameObject.transform.position = variables.phase1SelectedCharPositions[selectNum];
 				//gameObject.transform.localScale = new Vector3 (1.37f,1.37f,1);
-
+				
 				gameObject.GetComponent<BoxCollider>().size = new Vector3(5,3,0);
 				Vector3 center = gameObject.GetComponent<BoxCollider>().center;
 				//center.x -= 1.4f;
@@ -110,10 +110,8 @@ public class SelectScript : MonoBehaviour {
 				gameObject.GetComponent<BoxCollider>().center = center;
 				variables.currentCharacterSelectNum++;
 				if (variables.currentCharacterSelectNum == 2) {
-                    // There are two chars selected now. Set up high score text variables.selectedCharacters[0].name, variables.selectedCharacters[1].name, gameMode
-				    HighScore.text = "Previous Best: " +
-				                     ScoreManager.GetTopScore(variables.selectedCharacters[0].name,
-				                         variables.selectedCharacters[1].name);
+					// There are two chars selected now. Set up high score text
+					HighScore.text = "Previous Best: "  + ScoreManager.GetPlayerPrefsScore(variables.selectedCharacters[0].name, variables.selectedCharacters[1].name, gameMode);
 					if (HighScore.text == "Previous Best: " || HighScore.text == "Previous Best: 0")
 						HighScore.text = "";
 					Debug.Log("high score text = '"  + HighScore.text + "'");
@@ -122,7 +120,7 @@ public class SelectScript : MonoBehaviour {
 			//last check, if a player deselects a character that is already active
 			else {
 				thisSprite.sprite = standbyImage;
-
+				
 				gameObject.transform.position = startingSpot;
 				//gameObject.transform.localScale = new Vector3 (1.5f,1.5f,1);
 				//reverses the effects: moving gameObject back to original parent and removing it from arrays
@@ -134,14 +132,14 @@ public class SelectScript : MonoBehaviour {
 				selectNum = -1;
 				variables.currentCharacterSelectNum--;
 				character.charSelectOrder = -1;
-
-                // There aren't two characters selected, so delete high score text
-			    HighScore.text = "";
+				
+				// There aren't two characters selected, so delete high score text
+				HighScore.text = "";
 			}
 		}
-
+		
 	}
-
+	
 	void toggleSelect ()
 	{
 		//simply toggles the select bool whenever it is called.
@@ -153,7 +151,7 @@ public class SelectScript : MonoBehaviour {
 		}
 	}
 	
-
+	
 	public void loadMainGame () {
 		//		for (int i = 0; i < variables.characterSelectNum; i++) {
 		//			variables.selectedCharacters[i].transform.position = variables.phase2CharacterPositions[i];
@@ -161,7 +159,7 @@ public class SelectScript : MonoBehaviour {
 		variables.timeToChangeGameState = false;
 		PlayerPrefs.SetInt("Character 1", variables.selectedCharacterNums[0]);
 		PlayerPrefs.SetInt("Character 2", variables.selectedCharacterNums[1]);
-
+		
 		Application.LoadLevel("WordMaking");
 		//moves the characters into their appropriate positions
 	}
--- Unity Project/Assets/GUI/ScoreScreenAssets/Scripts/ReceiptGUI.cs
+++ Unity Project/Assets/GUI/ScoreScreenAssets/Scripts/ReceiptGUI.cs
@@ -7,235 +7,258 @@ using System;
 
 public class ReceiptGUI : MonoBehaviour
 {
-    #region Variables
-    public float RowOffset;
-    // The length of the receipt in Unity game units (without any rows)
-    public float EmptyReceiptLength;
-    // The lowest y coordinate the receipt should scroll to
-    public float LowestPoint;
-    // The distance between the top and bottom edges that the receipt is clamped to
-    public float ScreenOffset;
-    public GameObject RowPrefab;
-    public GameObject BottomInstance;
-    // The coordinates of the first row
-    public Vector3 FirstRowPos;
-    public TextMesh Character1Name;
-    public TextMesh Character2Name;
-	public TextMesh ScoreText; 
-	public TextMesh TilesDiscardedText;
-
-    int selectedCharacter1;
-    int selectedCharacter2;
-    public List<string> char1WordsFed;
-    public List<string> char2WordsFed;
-    // Stores the total number of rows, used to position the rows.
+	#region Variables
+	public TextMesh test;
+	public GUIStyle big;
+	public int characterNum;
+	public float rowOffset;
+	// A constant used to adjust how far down the receipt should scroll
+	public float EmptyReceiptLength;
+	// A constant used to adjust how far up the receipt should scroll
+	public Camera camera;
+	public float lowestPoint;
+	public float screenOffset;
+	public GameObject RowPrefab;
+	public GameObject bottomInstance;
+	public Vector3 firstRowPos;
+	public TextMesh Character1Name;
+	public TextMesh Character2Name;
+	public TextMesh scoreText; 
+	public TextMesh tilesDiscardedText;
+	
+	
+	int selectedCharacter1;
+	int selectedCharacter2;
+	public List <string> char1WordsFed = new List<string>();
+	public List <string> char2WordsFed =  new List<string>();
+	public Component[] meshes;
+	public float score;
 	public int rowCount;
-    #endregion
-
-    // Use this for initialization
-    void Start()
-    {
-        #region Receipt Setup
-        ScoreText.text = ((int)PlayerPrefs.GetFloat("Score")).ToString();
-		TilesDiscardedText.text = PlayerPrefs.GetInt("Trashed Letters").ToString();
-
-        //#region Get Receipt Info
-        rowCount = 0;
-        FirstRowPos = BottomInstance.transform.position;
-        Debug.Log("First row pos: " + FirstRowPos.ToString());
-        selectedCharacter1 = PlayerPrefs.GetInt("Character 1");
-        selectedCharacter2 = PlayerPrefs.GetInt("Character 2");
-
-        string char1String = Character.CharacterNameLookup[selectedCharacter1];
-        string char2String = Character.CharacterNameLookup[selectedCharacter2];        
-        if (selectedCharacter1 == null)
-            char1String = "Error";
-        if (selectedCharacter2 == null)
-            char2String = "Error";
-
-        Debug.Log("Char 1 " + char1String);
-        Debug.Log("Char 2 " + char2String);
-        Debug.Log(Application.persistentDataPath + " is the save file location");        
-        
-
-        Character1Name.text = char1String;
-        Character2Name.text = char2String;
-        
+	
+	//Ints that later get set from the values retrieved from Player Prefs
+	int rawScore;
+	int multiScore;
+	int trashLetterNum;
+	int trashedLetterScore;
+	#endregion
+	
+	// Use this for initialization
+	void Start()
+	{
+		#region Receipt Setup
+		scoreText.text = ((int)PlayerPrefs.GetFloat("Score")).ToString();
+		tilesDiscardedText.text = PlayerPrefs.GetInt("Trashed Letters").ToString();
+		
+		//#region Get Receipt Info
+		rowCount = 0;
+		firstRowPos = bottomInstance.transform.position;
+		Debug.Log("First row pos: " + firstRowPos.ToString());
+		selectedCharacter1 = PlayerPrefs.GetInt("Character 1");
+		selectedCharacter2 = PlayerPrefs.GetInt("Character 2");
+		
+		string char1String = Character.CharacterNameLookup[selectedCharacter1];
+		string char2String = Character.CharacterNameLookup[selectedCharacter2];        
+		if (selectedCharacter1 == null)
+			char1String = "Error";
+		if (selectedCharacter2 == null)
+			char2String = "Error";
+		
+		Debug.Log("Char 1 " + char1String);
+		Debug.Log("Char 2 " + char2String);
+		Debug.Log(Application.persistentDataPath + " is the save file location");        
+		
+		//        Debug.Log(ScoreManager.ToString());        
+		
+		Character1Name.text = char1String;
+		Character2Name.text = char2String;
 		if (GameObject.Find("WordsFed") != null) {
 			char1WordsFed = GameObject.Find("WordsFed").GetComponent<StoreWordsFed>().character1Words;
 			char2WordsFed = GameObject.Find("WordsFed").GetComponent<StoreWordsFed>().character2Words;
-        }
-
-        #region Add to WordsFed Manually
-        //char1WordsFed = new List<string>();
-        //char2WordsFed = new List<string>();
-
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        //char1WordsFed.Add("cat 30 5");
-        //char1WordsFed.Add("Nope 20 9");
-        #endregion
-        #endregion
-
-        #region Create wordsFed Rows
-        // Add code to create rows/fill them
-        int maxCount = Math.Max(char1WordsFed.Count, char2WordsFed.Count);
-        for(int i = 0; i < maxCount; i++)
-        {
-            string char1Word = "";
-            string char1Score = "";
-            string char2Word = "";
-            string char2Score = "";
-            string[] wordInfo;
-            if (i < char1WordsFed.Count)
-            {
-                wordInfo = char1WordsFed[i].Split(' ');
-                char1Word = wordInfo[0];
-                char1Score = (Convert.ToInt32(wordInfo[1])*Convert.ToInt32(wordInfo[2])).ToString();
-            }
-            if (i < char2WordsFed.Count)
-            {
-                wordInfo = char2WordsFed[i].Split(' ');
-                char2Word = wordInfo[0];
-                char2Score = (Convert.ToInt32(wordInfo[1]) * Convert.ToInt32(wordInfo[2])).ToString();
-            }
-            AddRow(char1Word, char1Score, char2Word, char2Score);
-        }
-        #endregion
-
-        #region High Score Sup
-        string gameMode = PlayerPrefs.GetInt("timed") == 1 ? "timed" : "casual";
-        Debug.Log("Current gamemode is " + gameMode);
-
-        // If the current score is the best score
-		if (ScoreManager.AddHighScore(char1String, char2String, gameMode, (int)PlayerPrefs.GetFloat("Score")))
-        {
-            Debug.Log("NEW HIGH SCORE: \n    " +
-                ((int)PlayerPrefs.GetFloat("Score")).ToString());
-			GameObject.Find("HighScoreText").GetComponent<TextMesh>().color = new Vector4(0.6F, 0, 0, 1);
-            GameObject.Find("HighScoreText").GetComponent<TextMesh>().text = 
-                "NEW HIGH SCORE: \n    " +
-				(int)PlayerPrefs.GetFloat("Score");
-        }
-        else // Find the previous best instead
-		{
-		    GameObject.Find("HighScoreText").GetComponent<TextMesh>().text =
-		        " HIGH SCORE: \n     " +
-		        ScoreManager.GetTopScore(char1String, char2String);
 		}
-
-        if (rowCount == 0)
-        {
-            AddRow("", "", "", "");
-            AddRow("", "", "", "");
-            AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");            
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-            //AddRow("", "", "", "");
-        }
-
-        #endregion
-
-        #region Setup Scroll Limits
-        ReceiptMover.lowestPos = LowestPoint;
-        ReceiptMover.highestPos = LowestPoint - ScreenOffset+ rowCount*RowOffset + EmptyReceiptLength;
-
-        if (ReceiptMover.highestPos <= ReceiptMover.lowestPos)
-        {
-            ReceiptMover.lowestPos = LowestPoint;
-            ReceiptMover.highestPos = LowestPoint;
-        }
-
-        Debug.Log("The receipt's highest point: " + ReceiptMover.highestPos);
-        Debug.Log("The receipt's lowest point: " + ReceiptMover.lowestPos);
-        Debug.Log("ROWCOUNT: " + rowCount);
-        #endregion 
-
-        #region Setup bottom
-        TilesDiscardedText.text = "0";
-
-        FirstRowPos.y = FirstRowPos.y - rowCount * RowOffset;
-        Vector3 pos = BottomInstance.transform.position;
-        pos.y -= rowCount*RowOffset;
-        BottomInstance.transform.position = pos;
-        BottomInstance.transform.parent = gameObject.transform;
-        #endregion
 		
-    }
-
-    // Given the strings to be displayed on a row, as well as the row number (starting at zero)
-    // this method creates a row with that text placed accordingly to the row number
-    public void AddRow(string char1Word, string char1Score, string char2Word, string char2Score)
-    {
-        int rowIndex = rowCount;
-        rowCount++;
-        Vector3 pos;
-        // Create a row
-        GameObject rowInstance = (GameObject)Instantiate(RowPrefab);
-        rowInstance.transform.parent = gameObject.transform;
-
-        // Move the row depending on how far down it is
-        pos = rowInstance.transform.position;
-        pos.y -= RowOffset * rowIndex;
-        rowInstance.transform.position = pos;
-
-        // Set the text fields as given
-        Component[] rowMeshes = rowInstance.GetComponentsInChildren<TextMesh>();
-        foreach (TextMesh mesh in rowMeshes)
-        {
-            switch (mesh.name)
-            {
-                case "char1Word":
-                    mesh.text = char1Word;
-                    break;
-                case "char1Score":
-                    mesh.text = char1Score;
-                    break;
-                case "char2Word":
-                    mesh.text = char2Word;
-                    break;
-                case "char2Score":
-                    mesh.text = char2Score;
-                    break;
-            }
-        }
-    }
-
-
+		//char1WordsFed = new List<string>();
+		//char2WordsFed = new List<string>();
+		
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		//char1WordsFed.Add("cat 30 5");
+		//char1WordsFed.Add("Nope 20 9");
+		
+		#endregion
+		
+		#region Create wordsFed Rows
+		// Add code to create rows/fill them
+		int maxCount = Math.Max(char1WordsFed.Count, char2WordsFed.Count);
+		for(int i = 0; i < maxCount; i++)
+		{
+			string char1Word = "";
+			string char1Score = "";
+			string char2Word = "";
+			string char2Score = "";
+			string[] wordInfo;
+			if (i < char1WordsFed.Count)
+			{
+				wordInfo = char1WordsFed[i].Split(' ');
+				char1Word = wordInfo[0];
+				char1Score = (Convert.ToInt32(wordInfo[1])*Convert.ToInt32(wordInfo[2])).ToString();
+			}
+			if (i < char2WordsFed.Count)
+			{
+				wordInfo = char2WordsFed[i].Split(' ');
+				char2Word = wordInfo[0];
+				char2Score = (Convert.ToInt32(wordInfo[1]) * Convert.ToInt32(wordInfo[2])).ToString();
+			}
+			AddRow(char1Word, char1Score, char2Word, char2Score);
+		}
+		#endregion
+		
+		#region High Score Sup
+		string gameMode = PlayerPrefs.GetInt("timed") == 1 ? "timed" : "casual";
+		Debug.Log("Current gamemode is " + gameMode);
+		
+		// If the current score is the best score
+		if (ScoreManager.CheckNewHighScore(char1String, char2String, gameMode, PlayerPrefs.GetFloat("Score")))
+		{
+			Debug.Log("NEW HIGH SCORE: \n    " +
+			          ((int)PlayerPrefs.GetFloat("Score")).ToString());
+			GameObject.Find("HighScoreText").GetComponent<TextMesh>().color = new Vector4(0.6F, 0, 0, 1);
+			GameObject.Find("HighScoreText").GetComponent<TextMesh>().text = 
+				"NEW HIGH SCORE: \n    " +
+					ScoreManager.GetPlayerPrefsScore(char1String, char2String, gameMode).ToString();
+		}
+		else // Find the previous best instead
+		{
+			GameObject.Find("HighScoreText").GetComponent<TextMesh>().text =
+				" HIGH SCORE: \n     " +
+					ScoreManager.GetPlayerPrefsScore(char1String, char2String, gameMode).ToString();
+		}
+		
+		if (rowCount == 0)
+		{
+			AddRow("", "", "", "");
+			AddRow("", "", "", "");
+			AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");            
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+			//AddRow("", "", "", "");
+		}
+		
+		#endregion
+		
+		#region Setup Scroll Limits
+		ReceiptMover.lowestPos = lowestPoint;
+		ReceiptMover.highestPos = lowestPoint - screenOffset+ rowCount*rowOffset + EmptyReceiptLength;
+		
+		if (ReceiptMover.highestPos <= ReceiptMover.lowestPos)
+		{
+			ReceiptMover.lowestPos = lowestPoint;
+			ReceiptMover.highestPos = lowestPoint;
+		}
+		
+		Debug.Log("The receipt's highest point: " + ReceiptMover.highestPos);
+		Debug.Log("The receipt's lowest point: " + ReceiptMover.lowestPos);
+		Debug.Log("ROWCOUNT: " + rowCount);
+		#endregion 
+		
+		#region Setup bottom
+		tilesDiscardedText.text = "0";
+		
+		firstRowPos.y = firstRowPos.y - rowCount * rowOffset;
+		Vector3 pos = bottomInstance.transform.position;
+		pos.y -= rowCount*rowOffset;
+		bottomInstance.transform.position = pos;
+		bottomInstance.transform.parent = gameObject.transform;
+		
+		
+		#endregion
+		if (GameObject.Find("WordsFed") != null) {
+			test.text = GameObject.Find("WordsFed").GetComponent<StoreWordsFed>().score.ToString();
+		}
+		//test.text = ScoreManager.ToString();
+	}
+	
+	// Given the strings to be displayed on a row, as well as the row number (starting at zero)
+	// this method creates a row with that text placed accordingly to the row number
+	public void AddRow(string char1Word, string char1Score, string char2Word, string char2Score)
+	{
+		int rowIndex = rowCount;
+		rowCount++;
+		Vector3 pos;
+		GameObject rowInstance = (GameObject)Instantiate(RowPrefab);
+		rowInstance.transform.parent = gameObject.transform;
+		pos = rowInstance.transform.position;
+		pos.y -= rowOffset * rowIndex;
+		rowInstance.transform.position = pos;
+		
+		Component[] rowMeshes = rowInstance.GetComponentsInChildren<TextMesh>();
+		
+		foreach (TextMesh mesh in rowMeshes)
+		{
+			switch (mesh.name)
+			{
+			case "char1Word":
+				mesh.text = char1Word;
+				break;
+			case "char1Score":
+				mesh.text = char1Score;
+				break;
+			case "char2Word":
+				mesh.text = char2Word;
+				break;
+			case "char2Score":
+				mesh.text = char2Score;
+				break;
+			}
+		}
+	}
+	
+	void Update()
+	{
+		//		test.text = "Rows: " + rowCount;
+		Vector3 currentPos = bottomInstance.transform.position;
+		//bottomInstance.transform.position = new Vector3(currentPos.x, gameObject.transform.position.y - ((rowCount + 3)* rowOffset), currentPos.z);
+	}
+	
+	//Method to display words fed
+	void DisplayWordsFed()
+	{
+		
+		
+	}
 }
