519454f86f67d784f26cf342428f27b8bfb1bd21
Ning Xie <ningxie1991@gmail.com>
2014-11-23 16:28:29 -0500

    cleaned up the about us move script

--- Unity Project/Assets/GUI/GUIScripts/AboutUsMove.cs
+++ Unity Project/Assets/GUI/GUIScripts/AboutUsMove.cs
@@ -5,14 +5,10 @@ public class AboutUsMove : MonoBehaviour {
 	Vector3 pos;
 	public float speed;
     public bool clickSound;
-	public BoxCollider boxCollider1;
-	public BoxCollider boxCollider2;
-	//Camera camera;
 
 	// Use this for initialization
 	void Start () {
 		pos = transform.localPosition;
-		//camera = GameObject.Find ("Main Camera").camera;
 	}
 	
 	// Update is called once per frame
@@ -20,16 +16,10 @@ public class AboutUsMove : MonoBehaviour {
 		if(pos.z > -8.86f ){
 			pos.z -= Time.deltaTime*speed;
 			transform.localPosition = pos;
-		}else{
-			//boxCollider1.GetComponent<BoxCollider>().enabled = false;
 		}
 	}
 
-	void OnMouseDown(){
-
-		//Application.OpenURL("http://wordsnack.net/");
-		//Application.LoadLevel ("StartScreenTest");
-			
+	void OnMouseDown(){	
 		speed = 18;
 		if(pos.z > -8.86f ){
 			pos.z -= Time.deltaTime*speed;
@@ -38,6 +28,5 @@ public class AboutUsMove : MonoBehaviour {
 			Application.LoadLevel ("StartScreenTest");
 		}
 		clickSound = true;
-		
 	}
 }
