cf660709cfcfbe2e134c3550449a18e13ce1123c
Wm. Josiah Erikson <josiah@insanetechnology.com>
2014-11-24 16:07:51 -0500

    Made Analytics submit less often. A lot less often.

--- Unity Project/Assets/GUI/GUIScripts/GoBackToMenu.cs
+++ Unity Project/Assets/GUI/GUIScripts/GoBackToMenu.cs
@@ -1,38 +1,45 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GoBackToMenu : MonoBehaviour {
-	public Sprite menuUnclicked;
-	public Sprite menuClicked;
-	public bool backToStart = false; //for PlayMusic.cs
-	SpriteRenderer s;
-	public bool clickSound;
+public class GoBackToMenu : MonoBehaviour
+{
+		public Sprite menuUnclicked;
+		public Sprite menuClicked;
+		public bool backToStart = false; //for PlayMusic.cs
+		SpriteRenderer s;
+		public bool clickSound;
 
-	// Use this for initialization
-	void Start () {
-		s = gameObject.GetComponent<SpriteRenderer>();
-		s.sprite = menuUnclicked;
-	}
+		// Use this for initialization
+		void Start ()
+		{
+				s = gameObject.GetComponent<SpriteRenderer> ();
+				s.sprite = menuUnclicked;
+		}
 	
-	// Update is called once per frame
-	void OnMouseDown () {
-		s.sprite = menuClicked;
-		transform.localPosition = new Vector3 (-2.50f, 1, 2.87297f);
+		// Update is called once per frame
+		void OnMouseDown ()
+		{
+				s.sprite = menuClicked;
+				transform.localPosition = new Vector3 (-2.50f, 1, 2.87297f);
 //		transform.localScale = new Vector3 (0.25f, 0.49f, 0.49f);
 
-	}
+		}
 
-	void OnMouseUp()
-	{
-		s.sprite = menuUnclicked;
+		void OnMouseUp ()
+		{
+				s.sprite = menuUnclicked;
 //		transform.localScale = new Vector3 (.15f, .3f, .49f);
-	}
-	void OnMouseUpAsButton()
-	{
-		s.sprite = menuUnclicked;
-		backToStart = true;
+		}
+		void OnMouseUpAsButton ()
+		{
+				s.sprite = menuUnclicked;
+				backToStart = true;			
+				//Get rid of the GameAnalytics objects so they don't submit things 7000 times
+				DestroyObject (GameObject.Find ("GA_SystemTracker"));
+				DestroyObject (GameObject.Find ("GA_AdSupport"));
+				DestroyObject (GameObject.Find ("GA_Controller"));
 //		ScoreLoadingScript.SetLoadingScreen();
-		Application.LoadLevel("StartScreenTest");
-		clickSound = true;
-	}
+				Application.LoadLevel ("StartScreenTest");
+				clickSound = true;
+		}
 }
--- Unity Project/Assets/GUI/ScoreScreenAssets/Scripts/PlayAgain.cs
+++ Unity Project/Assets/GUI/ScoreScreenAssets/Scripts/PlayAgain.cs
@@ -1,34 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayAgain : MonoBehaviour {
-	public Sprite PlayAgainPressed;
-    public Sprite PlayAgainUnpressed;
-    public bool clickSound;
-	// Use this for initialization
-	void Start () {
+public class PlayAgain : MonoBehaviour
+{
+		public Sprite PlayAgainPressed;
+		public Sprite PlayAgainUnpressed;
+		public bool clickSound;
+		// Use this for initialization
+		void Start ()
+		{
 	
-	}
+		}
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update ()
+		{
 	
-	}
+		}
 
-	void OnMouseDown () {
-		print ("clicked");
-		gameObject.GetComponent<SpriteRenderer>().sprite = PlayAgainPressed;
-	}
+		void OnMouseDown ()
+		{
+				print ("clicked");
+				gameObject.GetComponent<SpriteRenderer> ().sprite = PlayAgainPressed;
+		}
 
-    void OnMouseUp()
-    {
-        gameObject.GetComponent<SpriteRenderer>().sprite = PlayAgainUnpressed;
-    }
-    void OnMouseUpAsButton()
-    {
-        gameObject.GetComponent<SpriteRenderer>().sprite = PlayAgainUnpressed;
-        clickSound = true;
-		ScoreLoadingScript.SetLoadingScreen();
-        Application.LoadLevel("WordMaking");
-    }
+		void OnMouseUp ()
+		{
+				gameObject.GetComponent<SpriteRenderer> ().sprite = PlayAgainUnpressed;
+		}
+		void OnMouseUpAsButton ()
+		{
+				//Get rid of the GameAnalytics objects so they don't submit things 7000 times
+				DestroyObject (GameObject.Find ("GA_SystemTracker"));
+				DestroyObject (GameObject.Find ("GA_AdSupport"));
+				DestroyObject (GameObject.Find ("GA_Controller"));
+				gameObject.GetComponent<SpriteRenderer> ().sprite = PlayAgainUnpressed;
+				clickSound = true;
+				ScoreLoadingScript.SetLoadingScreen ();
+				Application.LoadLevel ("WordMaking");
+		}
 }
