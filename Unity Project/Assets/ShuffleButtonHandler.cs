dfc25b791e6250041526e5c89d2fd97d396c682e
David Carlson <britishorbust@gmail.com>
2014-11-26 18:22:50 -0500

    More script cleanup

--- Unity Project/Assets/ShuffleButtonHandler.cs
+++ Unity Project/Assets/ShuffleButtonHandler.cs
@@ -10,14 +10,9 @@ public class ShuffleButtonHandler : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		l = GameObject.Find ("letterGeneration").GetComponent<LetterController> ();
-	    gameObject.GetComponent<SpriteRenderer>().sprite = shuffleUnPressed;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
+    // Shuffle button is pressed - shuffle the letters
 	void OnMouseDown(){
 		l.shuffleLetters ();
 		gameObject.GetComponent<SpriteRenderer>().sprite = shufflePressed;
