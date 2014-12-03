147d84a51592001240cc0a5b7c7bc988174ffeef
David Carlson <britishorbust@gmail.com>
2014-12-03 13:37:01 -0500

    Hopefully fixed y being double counted

--- Unity Project/Assets/Characters/PlayerScripts/TasteCollection.cs
+++ Unity Project/Assets/Characters/PlayerScripts/TasteCollection.cs
@@ -19,7 +19,7 @@ public static class TasteCollection
     private static char[] consonants =
 	{
 		'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's',
-		't', 'v', 'w', 'x', 'y', 'z',
+		't', 'v', 'w', 'x', 'z',
 	};
     private static char[] vowels =
 	{
